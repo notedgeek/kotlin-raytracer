7f0e427ced7320f3b3ebd53747d974667efff1a0
seye <notedgeek@gmail.com>
2020-09-29 15:46:42 +0100

    matrix and transformations implemented

--- src/main/kotlin/com/notedgeek/rtace/math/linearAlgebra.kt
+++ src/main/kotlin/com/notedgeek/rtace/math/linearAlgebra.kt
@@ -2,9 +2,18 @@ package com.notedgeek.rtace.math
 
 import java.lang.IllegalArgumentException
 import kotlin.math.abs
+import kotlin.math.cos
+import kotlin.math.sin
 import kotlin.math.sqrt
 
 var EPSILON = 0.00001
+
+var I = Matrix(
+    1.0, 0.0, 0.0, 0.0,
+    0.0, 1.0, 0.0, 0.0,
+    0.0, 0.0, 1.0, 0.0,
+    0.0, 0.0, 0.0, 1.0
+)
 
 private fun closeTo(a: Double, b: Double) = abs(a - b) < EPSILON
 
@@ -18,6 +27,10 @@ class Point(val x: Double, val y: Double, val z: Double) {
 
     override fun equals(other: Any?) =
         other is Point && closeTo(x, other.x) && closeTo(y, other.y) && closeTo (z, other.z)
+
+    override fun toString(): String {
+        return "Point(x=$x, y=$y, z=$z)"
+    }
 }
 
 class Vector(val x: Double, val y: Double, val z: Double) {
@@ -40,7 +53,7 @@ class Vector(val x: Double, val y: Double, val z: Double) {
 
     fun mag() = sqrt(x * x + y * y + z * z)
 
-    fun norm() = this / mag()
+    fun normalise() = this / mag()
 
     infix fun dot(other: Vector) = x * other.x + y * other.y + z * other.z
 
@@ -52,9 +65,13 @@ class Vector(val x: Double, val y: Double, val z: Double) {
 
     override fun equals(other: Any?) =
         other is Vector && closeTo(x, other.x) && closeTo(y, other.y) && closeTo (z, other.z)
+
+    override fun toString(): String {
+        return "Vector(x=$x, y=$y, z=$z)"
+    }
 }
 
-open class Matrix(vararg val elems: Double) {
+open class Matrix(vararg elems: Double) {
 
     private val dim: Int = sqrt(elems.size.toDouble()).toInt()
     private val contents: Array<DoubleArray>
@@ -80,31 +97,110 @@ open class Matrix(vararg val elems: Double) {
         if (dim != other.dim) {
             throw IllegalArgumentException("cannot multiply different sized matrices this: $dim other: ${other.dim}")
         }
-        val result = DoubleArray(dim * dim)
-        var i = 0
+        val destArray = DoubleArray(dim * dim)
         for (r in 0 until dim) {
             for (c in 0 until dim) {
-                result[i] = getMultCell(r, c, other)
-                i++
+                destArray[r * dim + c] = getMultCell(r, c, other)
             }
         }
-        return Matrix(*result)
+        return Matrix(*destArray)
     }
 
-    private fun getMultCell(r: Int, c: Int, other: Matrix): Double {
-        var result = 0.0
+    operator fun times(p: Point) : Point {
+        val tuple = times(p.x, p.y, p.z, 1.0)
+        return Point(tuple[0], tuple[1], tuple[2])
+    }
+
+    operator fun times(v: Vector) : Vector {
+        val tuple = times(v.x, v.y, v.z, 0.0)
+        return Vector(tuple[0], tuple[1], tuple[2])
+    }
+
+    operator fun unaryMinus(): Matrix {
+        val det = det()
+        if (closeTo(det, 0.0)) {
+            throw IllegalArgumentException("un-invertable matrix $this")
+        }
+        val destArray = DoubleArray(dim * dim)
+        for (r in 0 until dim) {
+            for (c in 0 until dim) {
+                destArray[c * dim + r] = cofactor(r, c) / det
+            }
+        }
+        return Matrix(*destArray)
+    }
+
+    fun transpose(): Matrix {
+        val destArray = DoubleArray(dim * dim)
+        for (c in 0 until dim) {
+            for (r in 0 until dim) {
+                destArray[c * dim + r] = contents[r][c]
+            }
+        }
+        return Matrix(*destArray)
+    }
+
+    fun det(): Double =
+        if (dim == 2) {
+            contents[0][0] * contents [1][1] - contents[0][1] * contents[1][0]
+        } else {
+            var det = 0.0
+            for (i in 0 until dim) {
+                det += contents[0][i] * cofactor(0, i)
+            }
+            det
+        }
+
+    fun submatrix(rSkip: Int, cSkip: Int): Matrix {
+        if (dim < 2) {
+            throw IllegalArgumentException("matrix of dimension: $dim, too small to take sub matrix")
+        }
+        if (rSkip !in 0 until dim || cSkip !in 0 until dim) {
+            throw IllegalArgumentException("r or c out of range r: $rSkip c: $cSkip")
+        }
+        val destArray = DoubleArray((dim - 1) * (dim -1))
+        var i = 0
+        for (r in 0 until dim) {
+            if (r != rSkip) {
+                for (c in 0 until dim) {
+                    if(c != cSkip) {
+                        destArray[i] = contents[r][c]
+                        i++
+                    }
+                }
+            }
+        }
+        return Matrix(*destArray)
+    }
+
+    fun minor(r: Int, c: Int): Double {
+        if (dim < 3) {
+            throw IllegalArgumentException("dim must be 3 or higher, was: $dim")
+        }
+        return submatrix(r, c).det()
+    }
+
+    fun cofactor(r: Int, c: Int): Double = if ((r + c) % 2 == 1) -minor(r, c) else minor(r, c)
+
+    private fun times(x: Double, y: Double, z: Double, w: Double): DoubleArray {
+        if(dim != 4) {
+            throw IllegalArgumentException("matrix to multiply should be of dimension 4: $dim")
+        }
+        val result = DoubleArray(4)
+        val tuple =  doubleArrayOf(x, y, z, w)
         for(i in 0 until dim) {
-            result += contents[r][i] * other.contents[i][c]
+            result[i] = getTupleMultCell(i, tuple)
         }
         return result
     }
 
-    fun get(r: Int, c: Int) =
-        if(r !in 0 until dim || c !in 0 until dim) {
-            IllegalArgumentException("r or c out of range r: $r c: $c")
+    fun get(r: Int, c: Int): Double {
+        if (r !in 0 until dim || c !in 0 until dim) {
+            throw IllegalArgumentException("r or c out of range r: $r c: $c")
         } else {
-            contents[r][c]
+            return contents[r][c]
         }
+    }
 
     override fun equals(other: Any?): Boolean {
         if(other !is Matrix || other.dim != dim) {
@@ -129,7 +225,76 @@ open class Matrix(vararg val elems: Double) {
         }
         return sb.toString()
     }
+
+    private fun getMultCell(r: Int, c: Int, other: Matrix): Double {
+        var result = 0.0
+        for(i in 0 until dim) {
+            result += contents[r][i] * other.contents[i][c]
+        }
+        return result
+    }
+
+    private fun getTupleMultCell(r: Int, tuple: DoubleArray): Double {
+        var result = 0.0
+        for(i in 0 until dim) {
+            result += contents[r][i] * tuple[i]
+        }
+        return result
+    }
 }
 
+fun mag(v: Vector) =  v.mag()
 
+fun normalise(v: Vector) = v.normalise()
 
+fun transpose(m: Matrix) = m.transpose()
+
+fun det(m: Matrix) = m.det()
+
+fun submatrix(m: Matrix, r: Int, c: Int) = m.submatrix(r, c)
+
+fun minor(m: Matrix, r: Int, c: Int) = m.minor(r, c)
+
+fun cofactor(m: Matrix, r: Int, c: Int) = m.cofactor(r, c)
+
+fun translation(x: Double, y: Double, z: Double) = Matrix(
+    1.0, 0.0, 0.0, x,
+    0.0, 1.0, 0.0, y,
+    0.0, 0.0, 1.0, z,
+    0.0, 0.0, 0.0, 1.0
+)
+
+fun scaling(x: Double, y: Double, z: Double) = Matrix(
+    x, 0.0, 0.0, 0.0,
+    0.0, y, 0.0, 0.0,
+    0.0, 0.0, z, 0.0,
+    0.0, 0.0, 0.0, 1.0
+)
+
+fun rotationX(r: Double) = Matrix(
+    1.0, 0.0,     0.0,    0.0,
+    0.0, cos(r), -sin(r), 0.0,
+    0.0, sin(r),  cos(r), 0.0,
+    0.0, 0.0,     0.0,    1.0
+)
+
+fun rotationY(r: Double) = Matrix(
+    cos(r),  0.0, sin(r), 0.0,
+    0.0,     1.0, 0.0,    0.0,
+    -sin(r), 0.0, cos(r), 0.0,
+    0.0,     0.0, 0.0,    1.0
+)
+
+fun rotationZ(r: Double) = Matrix(
+    cos(r), -sin(r), 0.0, 0.0,
+    sin(r),  cos(r), 0.0, 0.0,
+    0.0,        0.0, 1.0, 0.0,
+    0.0,        0.0, 0.0, 1.0
+)
+
+fun shearing(xy: Double, xz: Double, yx: Double, yz: Double, zx: Double, zy: Double) = Matrix(
+    1.0, xy, xz,  0.0,
+    yx, 1.0, yz,  0.0,
+    zx, zy, 1.0,  0.0,
+    0.0, 0.0, 0.0, 1.0
+)
---
+++ src/test/kotlin/com/notedgeek/rtrace/math/TestMatrices.kt
@@ -0,0 +1,475 @@
+package com.notedgeek.rtrace.math
+
+import com.notedgeek.rtace.math.*
+import org.assertj.core.api.Assertions.assertThat
+import org.assertj.core.data.Offset
+import org.assertj.core.data.Offset.offset
+import org.junit.jupiter.api.Nested
+import org.junit.jupiter.api.Test
+import kotlin.math.PI
+import kotlin.math.sqrt
+
+private val SQ2 = sqrt(2.0)
+
+class MatrixTest {
+
+    @Nested
+    inner class BasicArithmetic {
+        @Test
+        fun `constructing and inspecting a 4 x 4 matrix`() {
+            val m = Matrix(
+                1.0, 2.0, 3.0, 4.0,
+                5.5, 6.5, 7.5, 8.5,
+                9.0, 10.0, 11.0, 12.0,
+                13.5, 15.5, 15.5, 16.5
+            )
+            assertThat(m.get(0, 0)).isEqualTo(1.0)
+            assertThat(m.get(0, 3)).isEqualTo(4.0)
+            assertThat(m.get(1, 0)).isEqualTo(5.5)
+            assertThat(m.get(1, 2)).isEqualTo(7.5)
+            assertThat(m.get(2, 2)).isEqualTo(11.0)
+            assertThat(m.get(3, 0)).isEqualTo(13.5)
+            assertThat(m.get(3, 2)).isEqualTo(15.5)
+        }
+
+        @Test
+        fun `multiplying two matrices`() {
+            val m1 = Matrix(
+                1.0, 2.0, 3.0, 4.0,
+                5.0, 6.0, 7.0, 8.0,
+                9.0, 8.0, 7.0, 6.0,
+                5.0, 4.0, 3.0, 2.0
+            )
+            val m2 = Matrix(
+                -2.0, 1.0, 2.0, 3.0,
+                3.0, 2.0, 1.0, -1.0,
+                4.0, 3.0, 6.0, 5.0,
+                1.0, 2.0, 7.0, 8.0
+            )
+            assertThat(m1 * m2).isEqualTo(
+                Matrix(
+                    20.0, 22.0, 50.0, 48.0,
+                    44.0, 54.0, 114.0, 108.0,
+                    40.0, 58.0, 110.0, 102.0,
+                    16.0, 26.0, 46.0, 42.0
+                )
+            )
+        }
+
+        @Test
+        fun `multiply matrix by point`() {
+            val m = Matrix(
+                1.0, 2.0, 3.0, 4.0,
+                2.0, 4.0, 4.0, 2.0,
+                8.0, 6.0, 4.0, 1.0,
+                0.0, 0.0, 0.0, 1.0
+            )
+            val p = Point(1, 2, 3)
+            assertThat(m * p).isEqualTo(Point(18, 24, 33))
+        }
+
+        @Test
+        fun `multiply matrix by vector`() {
+            val m = Matrix(
+                1.0, 2.0, 3.0, 4.0,
+                2.0, 4.0, 4.0, 2.0,
+                8.0, 6.0, 4.0, 1.0,
+                0.0, 0.0, 0.0, 1.0
+            )
+            val v = Vector(1, 2, 3)
+            assertThat(m * v).isEqualTo(Vector(14, 22, 32))
+        }
+
+        @Test
+        fun `multiply matrix by the identity matrix`() {
+            val m = Matrix(
+                0.0, 1.0, 2.0, 4.0,
+                1.0, 2.0, 4.0, 8.0,
+                2.0, 4.0, 8.0, 16.0,
+                4.0, 8.0, 16.0, 32.0
+            )
+            assertThat(m * I).isEqualTo(m)
+            assertThat(I * m).isEqualTo(m)
+        }
+
+        @Test
+        fun `multiply identity matrix by point`() {
+            val p = Point(1.0, -3.5, 6.0)
+            assertThat(I * p).isEqualTo(p)
+        }
+
+        @Test
+        fun `multiply identity matrix by vector`() {
+            val v = Vector(1.0, -3.5, 6.0)
+            assertThat(I * v).isEqualTo(v)
+        }
+
+        @Test
+        fun `transposing a matrix`() {
+            val m = Matrix(
+                0.0, 9.0, 3.0, 0.0,
+                9.0, 8.0, 0.0, 8.0,
+                1.0, 8.0, 5.0, 3.0,
+                0.0, 0.0, 5.0, 8.0
+            )
+            assertThat(transpose(m)).isEqualTo(
+                Matrix(
+                    0.0, 9.0, 1.0, 0.0,
+                    9.0, 8.0, 8.0, 0.0,
+                    3.0, 0.0, 5.0, 5.0,
+                    0.0, 8.0, 3.0, 8.0
+                )
+            )
+            assertThat(transpose(transpose(m))).isEqualTo(m)
+        }
+    }
+
+    @Nested
+    inner class Inverse {
+        @Test
+        fun `determinant of a 2x2 matrix`() {
+            val m = Matrix(
+                1.0, 5.0,
+                -3.0, 2.0
+            )
+            assertThat(det(m)).isCloseTo(17.0, offset(EPSILON))
+        }
+
+        @Test
+        fun `submatrix of a 3x3 matrix is a 2x2 matrix`() {
+            val m = Matrix(
+                1.0, 5.0, 0.0,
+                -3.0, 2.0, 7.0,
+                0.0, 6.0, -3.0
+            )
+            assertThat(submatrix(m, 0, 2)).isEqualTo(
+                Matrix(
+                    -3.0, 2.0,
+                    0.0, 6.0
+                )
+            )
+        }
+
+        @Test
+        fun `submatrix of a 4x4 matrix is a 3x3 matrix`() {
+            val m = Matrix(
+                -6.0, 1.0, 1.0, 6.0,
+                -8.0, 5.0, 8.0, 6.0,
+                -1.0, 0.0, 8.0, 2.0,
+                -7.0, 1.0, -1.0, 1.0
+            )
+            assertThat(submatrix(m, 2, 1)).isEqualTo(
+                Matrix(
+                    -6.0, 1.0, 6.0,
+                    -8.0, 8.0, 6.0,
+                    -7.0, -1.0, 1.0
+                )
+            )
+        }
+
+        @Test
+        fun `calculate minor of 3x3 matrix`() {
+            val m = Matrix(
+                3.0, 5.0, 0.0,
+                2.0, -1.0, -7.0,
+                6.0, -1.0, 5.0
+            )
+            val sm = submatrix(m, 1, 0)
+            assertThat(det(sm)).isCloseTo(25.0, offset(EPSILON))
+            assertThat(minor(m, 1, 0)).isCloseTo(25.0, offset(EPSILON))
+        }
+
+        @Test
+        fun `calculate cofactor of 3x3 matrix`() {
+            val m = Matrix(
+                3.0, 5.0, 0.0,
+                2.0, -1.0, -7.0,
+                6.0, -1.0, 5.0
+            )
+            assertThat(minor(m, 0, 0)).isCloseTo(-12.0, offset(EPSILON))
+            assertThat(cofactor(m, 0, 0)).isCloseTo(-12.0, offset(EPSILON))
+            assertThat(minor(m, 1, 0)).isCloseTo(25.0, offset(EPSILON))
+            assertThat(cofactor(m, 1, 0)).isCloseTo(-25.0, offset(EPSILON))
+        }
+
+        @Test
+        fun `calculate the determinant of a 3x3 matrix`() {
+            val m = Matrix(
+                1.0, 2.0, 6.0,
+                -5.0, 8.0, -4.0,
+                2.0, 6.0, 4.0
+            )
+            assertThat(cofactor(m, 0, 0)).isCloseTo(56.0, offset(EPSILON))
+            assertThat(cofactor(m, 0, 1)).isCloseTo(12.0, offset(EPSILON))
+            assertThat(cofactor(m, 0, 2)).isCloseTo(-46.0, offset(EPSILON))
+            assertThat(det(m)).isCloseTo(-196.0, offset(EPSILON))
+        }
+
+        @Test
+        fun `calculate the determinant of a 4x4 matrix`() {
+            val m = Matrix(
+                -2.0, -8.0, 3.0, 5.0,
+                -3.0, 1.0, 7.0, 3.0,
+                1.0, 2.0, -9.0, 6.0,
+                -6.0, 7.0, 7.0, -9.0
+            )
+            assertThat(cofactor(m, 0, 0)).isCloseTo(690.0, offset(EPSILON))
+            assertThat(cofactor(m, 0, 1)).isCloseTo(447.0, offset(EPSILON))
+            assertThat(cofactor(m, 0, 2)).isCloseTo(210.0, offset(EPSILON))
+            assertThat(cofactor(m, 0, 3)).isCloseTo(51.0, offset(EPSILON))
+            assertThat(det(m)).isCloseTo(-4071.0, offset(EPSILON))
+        }
+
+        @Test
+        fun `calculate the inverse of a 4x4 matrix`() {
+            val m = Matrix(
+                -5.0, 2.0, 6.0, -8.0,
+                1.0, -5.0, 1.0, 8.0,
+                7.0, 7.0, -6.0, -7.0,
+                1.0, -3.0, 7.0, 4.0
+            )
+            val mi = -m
+            assertThat(det(m)).isCloseTo(532.0, offset(EPSILON))
+            assertThat(cofactor(m, 2, 3)).isCloseTo(-160.0, offset(EPSILON))
+            assertThat(mi.get(3, 2)).isCloseTo(-160.0 / 532.0, offset(EPSILON))
+            assertThat(cofactor(m, 3, 2)).isCloseTo(105.0, offset(EPSILON))
+            assertThat(mi.get(2, 3)).isCloseTo(105.0 / 532.0, offset(EPSILON))
+            assertThat(mi).isEqualTo(
+                Matrix(
+                    0.21805, 0.45113, 0.24060, -0.04511,
+                    -0.80827, -1.45677, -0.44361, 0.52068,
+                    -0.07895, -0.22368, -0.05263, 0.19737,
+                    -0.52256, -0.81391, -0.30075, 0.30639
+                )
+            )
+        }
+
+        @Test
+        fun `calculate inverse of another 4x4 matrix`() {
+            val m = Matrix(
+                8.0, -5.0, 9.0, 2.0,
+                7.0, 5.0, 6.0, 1.0,
+                -6.0, 0.0, 9.0, 6.0,
+                -3.0, 0.0, -9.0, -4.0
+            )
+            assertThat(-m).isEqualTo(
+                Matrix(
+                    -0.15385, -0.15385, -0.28205, -0.53846,
+                    -0.07692, 0.12308, 0.02564, 0.03077,
+                    0.35897, 0.35897, 0.43590, 0.92308,
+                    -0.69231, -0.69231, -0.76923, -1.92308
+                )
+            )
+        }
+
+        @Test
+        fun `calculate inverse of a third 4x4 matrix`() {
+            val m = Matrix(
+                9.0, 3.0, 0.0, 9.0,
+                -5.0, -2.0, -6.0, -3.0,
+                -4.0, 9.0, 6.0, 4.0,
+                -7.0, 6.0, 6.0, 2.0
+            )
+            assertThat(-m).isEqualTo(
+                Matrix(
+                    -0.04074, -0.07778, 0.14444, -0.22222,
+                    -0.07778, 0.03333, 0.36667, -0.33333,
+                    -0.02901, -0.14630, -0.10926, 0.12963,
+                    0.17778, 0.06667, -0.26667, 0.33333
+                )
+            )
+        }
+    }
+
+    @Nested
+    inner class Translation {
+        @Test
+        fun `multiplying a point by a translation matrix`() {
+            val m = translation(1.0, -2.0, 3.0)
+            val p = Point(1, 2, 3)
+            assertThat(m * p).isEqualTo(Point(2, 0, 6))
+        }
+
+        @Test
+        fun `multiplying a point by the inverse of a translation matrix`() {
+            val m = translation(1.0, -2.0, 3.0)
+            val p = Point(1, 2, 3)
+            assertThat(-m * p).isEqualTo(Point(0, 4, 0))
+        }
+
+        @Test
+        fun `translating a vector has no effect`() {
+            val m = translation(1.0, -2.0, 3.0)
+            val v = Vector(1, 2, 3)
+            assertThat(m * v).isEqualTo(v)
+        }
+    }
+
+    @Nested
+    inner class Scaling {
+        @Test
+        fun `scaling matrix applied to a point`() {
+            val s = scaling(2.0, 3.0, 4.0)
+            val p = Point(-4, 6, 8)
+            assertThat(s * p).isEqualTo(Point(-8, 18, 32))
+        }
+
+        @Test
+        fun `scaling matrix applied to a vector`() {
+            val s = scaling(2.0, 3.0, 4.0)
+            val v = Vector(-4, 6, 8)
+            assertThat(s * v).isEqualTo(Vector(-8, 18, 32))
+        }
+
+        @Test
+        fun `multiplying by the inverse of a scaling matrix`() {
+            val s = scaling(2.0, 3.0, 4.0)
+            val v = Vector(-4, 6, 8)
+            assertThat(-s * v).isEqualTo(Vector(-2, 2, 2))
+        }
+    }
+
+    @Nested
+    inner class Rotation {
+        @Test
+        fun `rotating a point around the x axis`() {
+            val p = Point(0, 1, 0)
+            val halfQuarter = rotationX(PI / 4)
+            val fullQuarter = rotationX(PI / 2)
+            assertThat(halfQuarter * p).isEqualTo(Point(0.0, SQ2 / 2, SQ2 / 2))
+            assertThat(fullQuarter * p).isEqualTo(Point(0, 0, 1))
+        }
+
+        @Test
+        fun `rotating a vector around the x axis`() {
+            val v = Vector(0, 1, 0)
+            val halfQuarter = rotationX(PI / 4)
+            val fullQuarter = rotationX(PI / 2)
+            assertThat(halfQuarter * v).isEqualTo(Vector(0.0, SQ2 / 2, SQ2 / 2))
+            assertThat(fullQuarter * v).isEqualTo(Vector(0, 0, 1))
+        }
+
+        @Test
+        fun `inverse of an x rotation rotates in the opposite direction`() {
+            val p = Point(0, 1, 0)
+            val halfQuarter = rotationX(PI / 4)
+            assertThat(-halfQuarter * p).isEqualTo(Point(0.0, SQ2 / 2, -SQ2 / 2))
+
+        }
+
+        @Test
+        fun `rotating a point around the y axis`() {
+            val p = Point(0, 0, 1)
+            val halfQuarter = rotationY(PI / 4)
+            val fullQuarter = rotationY(PI / 2)
+            assertThat(halfQuarter * p).isEqualTo(Point(SQ2 / 2, 0.0, SQ2 / 2))
+            assertThat(fullQuarter * p).isEqualTo(Point(1, 0, 0))
+        }
+
+        @Test
+        fun `rotating a vector around the y axis`() {
+            val v = Vector(0, 0, 1)
+            val halfQuarter = rotationY(PI / 4)
+            val fullQuarter = rotationY(PI / 2)
+            assertThat(halfQuarter * v).isEqualTo(Vector(SQ2 / 2, 0.0, SQ2 / 2))
+            assertThat(fullQuarter * v).isEqualTo(Vector(1, 0, 0))
+        }
+
+        @Test
+        fun `inverse of an y rotation rotates in the opposite direction`() {
+            val p = Point(0, 0, 1)
+            val halfQuarter = rotationY(PI / 4)
+            assertThat(-halfQuarter * p).isEqualTo(Point(-SQ2 / 2, 0.0, SQ2 / 2))
+        }
+
+        @Test
+        fun `rotating a point around the z axis`() {
+            val p = Point(0, 1, 0)
+            val halfQuarter = rotationZ(PI / 4)
+            val fullQuarter = rotationZ(PI / 2)
+            assertThat(halfQuarter * p).isEqualTo(Point(-SQ2 / 2, SQ2 / 2, 0.0))
+            assertThat(fullQuarter * p).isEqualTo(Point(-1, 0, 0))
+        }
+
+        @Test
+        fun `rotating a vector around the z axis`() {
+            val v = Vector(0, 1, 0)
+            val halfQuarter = rotationZ(PI / 4)
+            val fullQuarter = rotationZ(PI / 2)
+            assertThat(halfQuarter * v).isEqualTo(Vector(-SQ2 / 2, SQ2 / 2, 0.0))
+            assertThat(fullQuarter * v).isEqualTo(Vector(-1, 0, 0))
+        }
+
+        @Test
+        fun `inverse of an z rotation rotates in the opposite direction`() {
+            val p = Point(0, 1, 0)
+            val halfQuarter = rotationZ(PI / 4)
+            assertThat(-halfQuarter * p).isEqualTo(Point(SQ2 / 2, SQ2 / 2, 0.0))
+        }
+    }
+
+    @Nested
+    inner class Shearing {
+        private var p = Point(2, 3, 4)
+
+        @Test
+        fun `shearing transformation moves x in proportion to y`() {
+            val t = shearing(1.0, 0.0, 0.0, 0.0, 0.0, 0.0)
+            assertThat(t * p).isEqualTo(Point(5, 3, 4))
+        }
+
+        @Test
+        fun `shearing transformation moves x in proportion to z`() {
+            val t = shearing(0.0, 1.0, 0.0, 0.0, 0.0, 0.0)
+            assertThat(t * p).isEqualTo(Point(6, 3, 4))
+        }
+
+        @Test
+        fun `shearing transformation moves y in proportion to x`() {
+            val t = shearing(0.0, 0.0, 1.0, 0.0, 0.0, 0.0)
+            assertThat(t * p).isEqualTo(Point(2, 5, 4))
+        }
+
+        @Test
+        fun `shearing transformation moves y in proportion to z`() {
+            val t = shearing(0.0, 0.0, 0.0, 1.0, 0.0, 0.0)
+            assertThat(t * p).isEqualTo(Point(2, 7, 4))
+        }
+
+        @Test
+        fun `shearing transformation moves z in proportion to x`() {
+            val t = shearing(0.0, 0.0, 0.0, 0.0, 1.0, 0.0)
+            assertThat(t * p).isEqualTo(Point(2, 3, 6))
+        }
+
+        @Test
+        fun `shearing transformation moves z in proportion to y`() {
+            val t = shearing(0.0, 0.0, 0.0, 0.0, 0.0, 1.0)
+            assertThat(t * p).isEqualTo(Point(2, 3, 7))
+        }
+    }
+
+    @Nested
+    inner class Chaining{
+        private val p = Point(1, 0, 1)
+        private val t1 = rotationX(PI / 2)
+        private val t2 = scaling(5.0, 5.0, 5.0)
+        private val t3 = translation(10.0, 5.0, 7.0)
+
+        @Test
+        fun `individual transformations are applied in sequence`() {
+            val p1 = t1 * p
+            assertThat(p1).isEqualTo(Point(1, -1, 0))
+            val p2 = t2 * p1
+            assertThat(p2).isEqualTo(Point(5, -5, 0))
+            val p3 = t3 * p2
+            assertThat(p3).isEqualTo(Point(15, 0, 7))
+        }
+
+        @Test
+        fun `chained transformations must be applied in erverse order`() {
+            val t = t3 * t2 * t1
+            assertThat(t * p).isEqualTo(Point(15, 0, 7))
+        }
+    }
+}
