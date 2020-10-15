61717066046fd59553d51a67c26f2d000d4dc376
scorcoran <corcoran_simon@hotmail.com>
2020-10-15 13:18:21 +0100

    square rendering chunks

--- src/main/kotlin/com/notedgeek/rtrace/graphics/pixelSourceRenderer.kt
+++ src/main/kotlin/com/notedgeek/rtrace/graphics/pixelSourceRenderer.kt
@@ -1,34 +1,63 @@
 package com.notedgeek.rtrace.graphics
 
 import java.io.IOException
+import java.time.Clock
 import java.util.*
+import kotlin.math.min
+import kotlin.system.exitProcess
+
+
+typealias Bound = Pair<Pair<Int, Int>, Pair<Int, Int>>
 
 class PixelSourceRenderer(private val pixelSource: PixelSource) : Renderer {
     internal inner class RenderRunner(
-        private val start: Int,
-        private val inc: Int,
+        private val bounds: Deque<Bound>,
         private val renderContext: RenderContext
     ) : Runnable {
         override fun run() {
-            for (y in 0 until renderContext.sizeY) {
-                var x = start
-                while (x < renderContext.sizeX) {
-                    val c = pixelSource.colorAt(x, y)
-                    renderContext.setRgb(
-                        x, y, c.red, c.green, c.blue
-                    )
-                    x += inc
+            while(true) {
+                val bound: Bound
+                synchronized(bounds) {
+                    if (bounds.isEmpty()) {
+                        return
+                    } else bound = bounds.removeFirst()
+                }
+                for (y in bound.first.second..bound.second.second) {
+                    for (x in bound.first.first..bound.second.first) {
+                        val c = pixelSource.colorAt(x, y)
+                        renderContext.setRgb(x, y, c.red, c.green, c.blue)
+                    }
                 }
             }
         }
     }
 
     override fun render(renderContext: RenderContext) {
+        val width = renderContext.sizeX
+        val height = renderContext.sizeY
+        val squareSize = 16
+        val boundsList = LinkedList<Bound>()
+        var x1 = 0
+        var y1 = 0
+        while(true) {
+            val x2 = min(x1 + squareSize - 1, width - 1)
+            val y2 = min(y1 + squareSize - 1, height - 1)
+            val chunk = Pair(x1 to y1, x2 to y2)
+            boundsList.add(chunk)
+            x1 = x2 + 1
+            if(x1 >= width) {
+                x1 = 0
+                y1 = y2 + 1
+                if(y1 >= height) {
+                    break
+                }
+            }
+        }
         val start = System.currentTimeMillis()
         val count = 8
         val threads: MutableList<Thread> = ArrayList()
         for (i in 0 until count) {
-            val thread = Thread(RenderRunner(i, count, renderContext))
+            val thread = Thread(RenderRunner(boundsList, renderContext))
             thread.start()
             threads.add(thread)
         }
